4d9cbbe5318cb534946dda0219521019c51976e0
manasveer6 <manasveer24@gmail.com>
2024-06-29 17:34:43 +0530

    streaks alignment

--- src/app/streak/page.tsx
+++ src/app/streak/page.tsx
@@ -13,24 +13,26 @@ export default function Streak() {
     <Container className="flex flex-wrap">
       <div className="w-1/2"></div>
       <div className="w-1/2">
-        <div className="streak">
-          <div className="flex items-center">
-            <Fire className="py-4" />
-            <p className="max-w-2xl py-4 text-lg leading-normal text-gray-500 lg:text-xl xl:text-xl dark:text-gray-300">
-              <h2 className="max-w-2xl mt-3 text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:leading-tight lg:text-4xl dark:text-white">
-                19
-              </h2>
-              daily streak
-            </p>
-          </div>
-          <div className="flex ">
-            <CheckMark />
-            <CheckMark />
-            <CheckMark />
-            <CheckMark />
-            <CheckMark />
-            <CheckMark />
-            <CheckMark />
+        <div className="streak flex flex-col justify-center items-center my-4">
+          <div className="container w-fit pb-4">
+            <div className="flex items-center">
+              <Fire className="pe-4" />
+              <p className="max-w-2xl pb-4 text-lg leading-normal text-gray-500 lg:text-xl xl:text-xl dark:text-gray-300">
+                <h2 className="max-w-2xl mt-3 text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:leading-tight lg:text-4xl dark:text-white">
+                  19
+                </h2>
+                daily streak
+              </p>
+            </div>
+            <div className="flex gap-x-2">
+              <CheckMark day="M" />
+              <CheckMark day="T" />
+              <CheckMark day="W" />
+              <CheckMark day="T" />
+              <CheckMark day="F" />
+              <CheckMark day="S" />
+              <CheckMark day="S" />
+            </div>
           </div>
         </div>
         <div>
