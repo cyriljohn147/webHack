fb69d5afc3fdebb8e85880096f292358046ddc34
NangunooriAbhinav <abhinavnangunoori.29@gmail.com>
2024-06-30 00:41:03 +0000

    added streak button

--- src/app/streak/page.tsx
+++ src/app/streak/page.tsx
@@ -12,13 +12,19 @@ import { useState } from "react";
 export default function Streak() {
   const [curDate, setCurDate] = useState<Date>(new Date("2022-06-21"));
   const [stage, setStage] = useState<number>(0);
+  const [streak, setStreak] = useState<number>(0);
 
   const getStreakDate = () => {
     return dayjs(curDate).add(19, "day");
   };
 
+  const handleClick = () => {
+    setCurDate(new Date());
+    setStreak((prev) => prev + 1);
+  };
+
   return (
-    <Container className="flex flex-wrap ">
+    <Container className="flex flex-wrap justify-center">
       <div className="w-1/2 flex justify-center items-center">
         {stage === 0 && <img className="w-1/2" src="treeSapling.svg"></img>}
         {stage === 1 && <img className="w-1/2" src="secondStage.svg"></img>}
@@ -60,6 +66,15 @@ export default function Streak() {
             </DemoContainer>
           </LocalizationProvider>
         </div>
+      </div>
+      <div>
+        <button
+          type="button"
+          className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800"
+          onClick={handleClick}
+        >
+          ADD TO STREAK!
+        </button>
       </div>
     </Container>
   );
